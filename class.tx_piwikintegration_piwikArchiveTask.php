48ddcdc070a6c04350a4468a05358fc21f6c8a68
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-13 20:25:35 +0000

    added fixes to scheduler, there currently performance issues with piwik, they are known in piwikbugtracker
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/piwikintegration/trunk@27662 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_piwikintegration_piwikArchiveTask.php
+++ class.tx_piwikintegration_piwikArchiveTask.php
@@ -2,6 +2,9 @@
 require_once(t3lib_extMgm::extPath('scheduler', 'class.tx_scheduler_task.php'));
 class tx_piwikintegration_piwikArchiveTask extends tx_scheduler_Task {
 	public function execute() { 
+		//set execution time
+		ini_set('max_execution_time',0);
+		//find piwik
 		$piwikScriptPath = dirname(dirname(__FILE__)).'/../piwik/piwik';
 		define('PIWIK_INCLUDE_PATH', $piwikScriptPath);
 		define('PIWIK_ENABLE_DISPATCH', false);
@@ -11,7 +14,11 @@ class tx_piwikintegration_piwikArchiveTask extends tx_scheduler_Task {
 		Piwik_FrontController::getInstance()->init();
 		
 		$piwikConfig = parse_ini_file($piwikScriptPath.'/config/config.ini.php');
-		
+
+		//log		
+		$this->writeLog(
+			'EXT:piwikintegration cronjob'
+		);
 		//get API key
 		$request = new Piwik_API_Request('
 			module=API
@@ -21,24 +28,68 @@ class tx_piwikintegration_piwikArchiveTask extends tx_scheduler_Task {
 			&format=php
 			&serialize=0'
 		);
-		
 		$TOKEN_AUTH = $request->process();
-		//create Archive in piwik
-		$periods = array('day','week','year');
-		foreach($periods as $period) {
-			$request = new Piwik_API_Request('
-			            module=API
-						&method=VisitsSummary.getVisits
-						&idSite=all
-						&period='.$period.'
-						&date=last52
-						&format=xml
-						&token_auth='.$TOKEN_AUTH.'"
-			');
-			$request->process();
-		}
 
+		//get all piwik siteid's
+		$request = new Piwik_API_Request('
+			module=API
+			&method=SitesManager.getSitesWithAdminAccess
+			&token_auth='.$TOKEN_AUTH.'"
+			&format=php
+			&serialize=0'
+		);
+		$piwikSiteIds = $request->process();
+		//log
+		$this->writeLog(
+			'EXT:piwikintegration got '.count($piwikSiteIds).' siteid´s and piwik token, start archiving '
+		);
+		
+		//create Archive in piwik
+		$periods = array(
+			'day',
+			'week',
+			'month',
+			#'year',
+		);
+		//iterate through sites
+		//can be done with allSites, but this cannot create the logentries
+		foreach($periods as $period) {
+			foreach($piwikSiteIds as $siteId) {
+				$starttime = microtime(true);
+				$request = new Piwik_API_Request('
+				            module=API
+							&method=VisitsSummary.getVisits
+							&idSite='.intval($siteId['idsite']).'
+							&period='.$period.'
+							&date=last52
+							&format=xml
+							&token_auth='.$TOKEN_AUTH.'"
+				');
+				$request->process();
+				//log
+				$this->writeLog(
+					'EXT:piwikintegration period '.$period.' ('.$siteId['idsite'].') '.$siteId['name'].' ('.round(microtime(true)-$starttime,3).'s)'
+				);
+			}
+		}
+		//log		
+		$this->writeLog(
+			'EXT:piwikintegration cronjob ended'
+		);
 		return true;
+	}
+	function writeLog($message,$data='') {
+		$conf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['piwikintegration']);
+		if($conf['enableSchedulerLoging']) {
+			$GLOBALS['BE_USER']->writeLog(
+				4,	//extension
+				0,	//no categorie
+				0,	//message
+				0,	//messagenumber
+				$message,
+				$data
+			);
+		}
 	}
 }
 ?>
