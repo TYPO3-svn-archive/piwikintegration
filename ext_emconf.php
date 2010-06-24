be3d379f74fa2a99f089d0bb33a2f3d7b325978e
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-24 05:03:02 +0000

    V 2.3.1 after TER Upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/piwikintegration/trunk@34850 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "piwikintegration".
 #
-# Auto generated 16-01-2010 13:01
+# Auto generated 23-06-2010 13:21
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Uses EXT:piwik to inserts Data in the HTML header and gives BE-Users the right to see the data for their sites. Autoupdate of Piwik will work as TYPO3-Admin!',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '2.3.0',
+	'version' => '2.3.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:42:{s:9:"ChangeLog";s:4:"54ba";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_piwikintegration.php";s:4:"fa36";s:38:"class.tx_piwikintegration_be_users.php";s:4:"2e64";s:36:"class.tx_piwikintegration_helper.php";s:4:"86c8";s:46:"class.tx_piwikintegration_piwikArchiveTask.php";s:4:"50ca";s:16:"ext_autoload.php";s:4:"56fe";s:21:"ext_conf_template.txt";s:4:"be78";s:12:"ext_icon.gif";s:4:"d803";s:17:"ext_localconf.php";s:4:"2d0e";s:15:"ext_php_api.dat";s:4:"3a12";s:14:"ext_tables.php";s:4:"6ae5";s:14:"ext_tables.sql";s:4:"d7e4";s:13:"locallang.xml";s:4:"6861";s:16:"locallang_db.xml";s:4:"f8e2";s:14:"doc/manual.sxw";s:4:"06bd";s:19:"doc/wizard_form.dat";s:4:"65a4";s:20:"doc/wizard_form.html";s:4:"fd83";s:13:"mod1/conf.php";s:4:"2359";s:14:"mod1/index.php";s:4:"e7ac";s:18:"mod1/locallang.xml";s:4:"d425";s:22:"mod1/locallang_mod.xml";s:4:"e238";s:22:"mod1/mod_template.html";s:4:"ee0a";s:19:"mod1/moduleicon.gif";s:4:"d803";s:14:"pi1/ce_wiz.gif";s:4:"0ee1";s:37:"pi1/class.tx_piwikintegration_pi1.php";s:4:"f32a";s:57:"pi1/class.tx_piwikintegration_pi1_templavoila_preview.php";s:4:"64a9";s:45:"pi1/class.tx_piwikintegration_pi1_wizicon.php";s:4:"d0f6";s:19:"pi1/flexform_ds.xml";s:4:"ab44";s:17:"pi1/locallang.xml";s:4:"ff18";s:35:"piwik_patches/config/config.ini.php";s:4:"af0b";s:41:"piwik_patches/plugins/TYPO3Login/Auth.php";s:4:"fe54";s:47:"piwik_patches/plugins/TYPO3Login/Controller.php";s:4:"004b";s:47:"piwik_patches/plugins/TYPO3Login/TYPO3Login.php";s:4:"d04c";s:46:"piwik_patches/plugins/TYPO3Menu/Controller.php";s:4:"48c3";s:45:"piwik_patches/plugins/TYPO3Menu/TYPO3Menu.php";s:4:"9def";s:44:"piwik_patches/plugins/TYPO3Menu/css/main.css";s:4:"7eb9";s:45:"piwik_patches/plugins/TYPO3Menu/css/typo3.css";s:4:"bd22";s:42:"piwik_patches/plugins/TYPO3Menu/js/main.js";s:4:"581d";s:57:"piwik_patches/plugins/TYPO3Menu/pics/login-box-header.png";s:4:"43f3";s:38:"static/piwik_integration/constants.txt";s:4:"d41d";s:34:"static/piwik_integration/setup.txt";s:4:"ab5e";}',
+	'_md5_values_when_last_written' => 'a:44:{s:9:"ChangeLog";s:4:"54ba";s:10:"README.txt";s:4:"ee2d";s:29:"class.tx_piwikintegration.php";s:4:"fa36";s:38:"class.tx_piwikintegration_be_users.php";s:4:"2e64";s:36:"class.tx_piwikintegration_helper.php";s:4:"59f2";s:46:"class.tx_piwikintegration_piwikArchiveTask.php";s:4:"50ca";s:16:"ext_autoload.php";s:4:"56fe";s:21:"ext_conf_template.txt";s:4:"be78";s:12:"ext_icon.gif";s:4:"d803";s:17:"ext_localconf.php";s:4:"2d0e";s:15:"ext_php_api.dat";s:4:"3a12";s:14:"ext_tables.php";s:4:"6ae5";s:14:"ext_tables.sql";s:4:"d7e4";s:13:"locallang.xml";s:4:"6861";s:16:"locallang_db.xml";s:4:"f8e2";s:14:"doc/manual.pdf";s:4:"ad3e";s:14:"doc/manual.sxw";s:4:"8b67";s:14:"doc/manual.txt";s:4:"54c8";s:19:"doc/wizard_form.dat";s:4:"65a4";s:20:"doc/wizard_form.html";s:4:"fd83";s:13:"mod1/conf.php";s:4:"2359";s:14:"mod1/index.php";s:4:"e7ac";s:18:"mod1/locallang.xml";s:4:"d425";s:22:"mod1/locallang_mod.xml";s:4:"e238";s:22:"mod1/mod_template.html";s:4:"ee0a";s:19:"mod1/moduleicon.gif";s:4:"d803";s:14:"pi1/ce_wiz.gif";s:4:"0ee1";s:37:"pi1/class.tx_piwikintegration_pi1.php";s:4:"f32a";s:57:"pi1/class.tx_piwikintegration_pi1_templavoila_preview.php";s:4:"64a9";s:45:"pi1/class.tx_piwikintegration_pi1_wizicon.php";s:4:"d0f6";s:19:"pi1/flexform_ds.xml";s:4:"ab44";s:17:"pi1/locallang.xml";s:4:"ff18";s:35:"piwik_patches/config/config.ini.php";s:4:"3bb1";s:41:"piwik_patches/plugins/TYPO3Login/Auth.php";s:4:"aa56";s:47:"piwik_patches/plugins/TYPO3Login/Controller.php";s:4:"004b";s:47:"piwik_patches/plugins/TYPO3Login/TYPO3Login.php";s:4:"0d45";s:46:"piwik_patches/plugins/TYPO3Menu/Controller.php";s:4:"48c3";s:45:"piwik_patches/plugins/TYPO3Menu/TYPO3Menu.php";s:4:"9def";s:44:"piwik_patches/plugins/TYPO3Menu/css/main.css";s:4:"7eb9";s:45:"piwik_patches/plugins/TYPO3Menu/css/typo3.css";s:4:"bd22";s:42:"piwik_patches/plugins/TYPO3Menu/js/main.js";s:4:"581d";s:57:"piwik_patches/plugins/TYPO3Menu/pics/login-box-header.png";s:4:"43f3";s:38:"static/piwik_integration/constants.txt";s:4:"d41d";s:34:"static/piwik_integration/setup.txt";s:4:"ab5e";}',
 	'suggests' => array(
 	),
 );
