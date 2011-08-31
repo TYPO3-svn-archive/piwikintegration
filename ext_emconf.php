7c1af08ab01e0dcf70641153b735d3d47f1f70d5
kaystrobach <kaystrobach@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-31 13:19:54 +0000

    [TASK] make 4.6 compatible
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/piwikintegration/trunk@51592 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-6.0.0',
-			'typo3' => '4.4.0-4.5.99',
+			'typo3' => '4.4.0-4.6.99',
 		),
 		'conflicts' => array(
 			'typo3' => '3.0.0-4.3.99',
